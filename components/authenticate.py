16134282d7af3c4aa70839f8277e9e6b7d958dba
Gabo <gabriel.vapp@gmail.com>
2024-01-25 12:58:49 -0500

    auth updated4

--- components/authenticate.py
+++ components/authenticate.py
@@ -33,42 +33,42 @@ def initialise_st_state_vars():
 # ----------------------------------
 # Get authorization code after login
 # ----------------------------------
-# def get_auth_code():
-#     """
-#     Gets auth_code state variable.
-
-#     Returns:
-#         Nothing.
-#     """
-#     # auth_query_params = st.query_params
-#     auth_query_params = st.experimental_get_query_params()
-#     print(auth_query_params)  # checando esta linea y la de arriba
-#     try:
-#         auth_code = dict(auth_query_params)["code"][0]
-#     except (KeyError, TypeError):
-#         auth_code = ""
-
-#     return auth_code
-        
 def get_auth_code():
     """
     Gets auth_code state variable.
 
     Returns:
-        The authorization code if present in the query parameters, otherwise an empty string.
+        Nothing.
     """
-    
-    # Access st.query_params as a property, not as a function
-    auth_query_params = st.query_params
-    
-    # Access the 'code' parameter directly from auth_query_params
+    # auth_query_params = st.query_params
+    auth_query_params = st.experimental_get_query_params()
+    print(auth_query_params)  # checando esta linea y la de arriba
     try:
-        # If 'code' is present, get the first value, otherwise default to an empty string
-        auth_code = auth_query_params.get("code", [""])[0]
+        auth_code = dict(auth_query_params)["code"][0]
     except (KeyError, TypeError):
         auth_code = ""
-    print(auth_code)
+
     return auth_code
+        
+# def get_auth_code():
+#     """
+#     Gets auth_code state variable.
+
+#     Returns:
+#         The authorization code if present in the query parameters, otherwise an empty string.
+#     """
+    
+#     # Access st.query_params as a property, not as a function
+#     auth_query_params = st.query_params
+    
+#     # Access the 'code' parameter directly from auth_query_params
+#     try:
+#         # If 'code' is present, get the first value, otherwise default to an empty string
+#         auth_code = auth_query_params.get("code", [""])[0]
+#     except (KeyError, TypeError):
+#         auth_code = ""
+#     print(auth_code)
+#     return auth_code
 
 # ----------------------------------
 # Set authorization code after login
